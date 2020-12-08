cb5d947889cea0bcfb72cd60c62bb5f4671959f9
dereyurtali <ali.dereyurt@stu.fsm.edu.tr>
2020-12-09 01:32:33 +0300

    created

--- Chess_Pieces_Moves/main.c
+++ Chess_Pieces_Moves/main.c
@@ -1,14 +1,6 @@
-//
-//  main.c
-//  Chess_Pieces_Moves
-//
-//  Created by Ali D. on 9.12.2020.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    
     return 0;
 }
