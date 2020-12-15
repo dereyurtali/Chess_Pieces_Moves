06e5adf3bd71dbebd5d4e9134a2ae856bc4ac431
dereyurtali <ali.dereyurt@stu.fsm.edu.tr>
2020-12-15 17:21:53 +0300

    creating a func for print possible moves. will make the details of printing later.

--- Chess_Pieces_Moves/ChessLib.c
+++ Chess_Pieces_Moves/ChessLib.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+struct Poz{
+    short yatay;
+    char dusey;
+};
+
+
+
 int **createChessTable(){
     int i, j;
     int **table = (int **)calloc(8, sizeof(int *));
@@ -12,8 +21,7 @@ int **createChessTable(){
         for (j = 0; j < 8; j++) {
             table[i][j] = 0;
         }
-    }
-    return table;
+    }return table;
 }
 
 void printChessTable(int **table){
@@ -24,5 +32,25 @@ void printChessTable(int **table){
         }
         puts("");
     }
-    
+}
+
+
+void hareketYazdir(char tas, struct Poz ilkPozisyon){
+    struct Poz *(*satrancFonksiyonlari[]) (struct Poz) = {&hareketAt, &hareketFil, //  creating array of
+                                                          &hareketSah, &hareketKale,// func. pointer
+                                                          &hareketPiyon, &hareketVezir};
+    switch (tas){
+        case 'a':
+            satrancFonksiyonlari[0] (ilkPozisyon);break;
+        case 'f':
+            satrancFonksiyonlari[1] (ilkPozisyon);break;
+        case 's':
+            satrancFonksiyonlari[2] (ilkPozisyon);break;
+        case 'k':
+            satrancFonksiyonlari[3] (ilkPozisyon);break;
+        case 'p':
+            satrancFonksiyonlari[4] (ilkPozisyon);break;
+        case 'v':
+            satrancFonksiyonlari[5] (ilkPozisyon);break;
+    }
 }
--- Chess_Pieces_Moves/ChessLib.h
+++ Chess_Pieces_Moves/ChessLib.h
@@ -5,6 +5,22 @@
 
 #endif /* ChessLib_h */
 
+struct Poz{};
+
 int **createChessTable(void);
 
 void printChessTable(int **);
+
+void hareketYazdir(char, struct Poz);
+
+struct Poz *hareketSah(struct Poz);
+
+struct Poz *hareketVezir(struct Poz);
+
+struct Poz *hareketFil(struct Poz);
+
+struct Poz *hareketAt(struct Poz);
+
+struct Poz *hareketKale(struct Poz);
+
+struct Poz *hareketPiyon(struct Poz);
