fd595d97ef859874019bcbf36c6d230fa0a68914
Ali DEREYURT <ali.dereyurt@stu.fsm.edu.tr>
2020-12-20 03:12:17 +0300

    finished

--- Chess_Pieces_Moves/main.c
+++ Chess_Pieces_Moves/main.c
@@ -6,8 +6,9 @@ int main() {
     fav.dusey = 'e';
     fav.yatay = 4;
     char tas;
-    printf("hangi tur tasi gormek istersiniz?\n");
+    printf("At icin 'a', Fil icin 'f', Sah icin 's', \nKale icin 'k', Piyon icin 'p', Vezir icin 'v' giriniz:\n");
     scanf("%c",&tas);
+    printf("\n\n");
     hareketYazdir(tas, fav);
     return 0;
 }
